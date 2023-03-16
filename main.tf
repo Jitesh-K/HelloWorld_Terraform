bdf29b1a6c888d6256814a482bf165988676dd6d
Jitesh Kumar <jiteshj.kumar@gmail.com>
2023-03-17 00:04:27 +0530

    adding aws vpc and subnet

--- main.tf
+++ main.tf
@@ -21,3 +21,18 @@ resource "aws_instance" "hello-world" {
     Name = "ubuntu"
   }
 }
+
+resource "aws_vpc" "vpc" {
+  cidr_block       = "10.0.0.0/16"
+  tags = {
+    Name = "vpc"
+  }
+}
+
+resource "aws_subnet" "subnet-1" {
+  vpc_id     = aws_vpc.vpc.id
+  cidr_block = "10.0.1.0/24"
+  tags = {
+    Name = "vpc-subnet"
+  }
+}
